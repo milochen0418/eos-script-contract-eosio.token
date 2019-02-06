49be24cfe0b931ca0f403f4b43cdcb370c9fccbb
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-06 14:47:17 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#eosio-cpp -I include -o eosio.token.wasm src/eosio.token.cpp --abigen
+#eosio-cpp -I include -o $CONTRACT_NAME.wasm src/$CONTARCT_NAME.cpp --abigen
+
 source $(dirname $0)/setting.sh
 #CONTRACT_NAME=eosio.token
 #BUILD_DIR=build
@@ -7,9 +10,13 @@ COMPILE_COMMANDS=$(echo -e "eosio-cpp -I include -o $CONTRACT_NAME.wasm src/$CON
 echo "$COMPILE_COMMANDS"
 eval $COMPILE_COMMANDS
 
-#eosio-cpp -I include -o eosio.token.wasm src/eosio.token.cpp --abigen
-#eosio-cpp -I include -o $CONTRACT_NAME.wasm src/$CONTARCT_NAME.cpp --abigen
-echo "Don't forget to do the following"
+rm -rf ./$BUILD_DIR
+mkdir -p ./$BUILD_DIR
+mv $CONTRACT_NAME.wasm ./$BUILD_DIR/$BUILD_DIR.wasm
+mv $CONTRACT_NAME.abi ./$BUILD_DIR/$BUILD_DIR.abi
+echo "Have moved built result into ./$BUILD_DIR for deploy.sh to deploy"
+
+echo "Don't forget to do the following before use ./deploy.sh"
 echo "===================================="
 echo "source ~/eos-script/nodeos/use-eos-script.sh"
 echo "createAccount.sh eosio.token"
